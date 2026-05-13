ec63b04880f42f91631c823cab98bc0b7e80f19b
Jack Deardorff <jackdeardorff@jacks-mbp.mynetworksettings.com>
2026-05-13 18:12:17 -0400

    Add scroll to top on page navigation

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { useEffect } from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
 export function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
